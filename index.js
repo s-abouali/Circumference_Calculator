9fbf96191669fd1610f9056767828d68ca8a0550
Sarah Abouali <sarahabouali7@gmail.com>
2026-06-29 17:56:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const PI = 3.14159;
 let radius;
 let circumference;
 
-radius = window.prompt("enter the radius of a circle");
-radius = Number(radius);
-
-circumference = 2 * PI * radius;
-
 document.getElementById("mySubmit").onclick = function(){
     radius = document.getElementById("myText").value;
     radius = Number(radius);
