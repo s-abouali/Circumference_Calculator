23f90154216fd22ac2b280f1e5a7224bb62da9f5
Sarah Abouali <sarahabouali7@gmail.com>
2026-06-29 17:54:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ radius = Number(radius);
 
 circumference = 2 * PI * radius;
 
-console.log(circumference);
+document.getElementById("mySubmit").onclick = function(){
+    radius = document.getElementById("myText").value;
+    radius = Number(radius);
+    circumference = 2 * PI * radius;
+    document.getElementById("myH3").textContent = `Your circumference is ${circumference}`;
+}
